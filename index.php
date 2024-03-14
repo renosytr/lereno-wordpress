67cedc23f350cd900f56ed24a436b7680013d26a
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-03-14 08:28:22 +0700

    firstl commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Lereno
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lereno</title>
+ </head>
+ <body>
+  index
+ </body>
+ </html>
